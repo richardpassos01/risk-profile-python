4962a830658dbc1ca11f36e47044ab7ec14361b3
richardpassos01 <richard.passos@hotmail.com>
2020-12-06 23:42:23 -0300

    feat: test python

--- risk_profile/application/controllers/RiskProfileViewSet.py
+++ risk_profile/application/controllers/RiskProfileViewSet.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
+from risk_profile.domain.suitability.use_cases import provide_risk_profile
+from risk_profile.domain.suitability.serializer import SuitabilitySerializer
+from rest_framework.response import Response
 from risk_profile.domain.suitability import Suitability as Model
-from risk_profile.domain.suitability import serializer
-
 
 class RiskProfileViewSet(viewsets.ModelViewSet):
     queryset = Model.Suitability.objects.all()
-    serializer_class = serializer.SuitabilitySerializer
+    serializer_class = SuitabilitySerializer
+
+    def create(self, request):
+        use_case = provide_risk_profile.ProvideRiskProfile()
+        risk_profile = use_case.execute(request.data)
+       
+        serializer = SuitabilitySerializer(queryset, many=True)
+        return Response(serializer.data)
---
+++ risk_profile/domain/suitability/use_cases/provide_risk_profile.py
@@ -0,0 +1,9 @@
+from risk_profile.domain.suitability import Suitability as Model
+
+
+class ProvideRiskProfile:
+    def execute(self, data):
+        try:
+            return "richard"
+        except True:
+            return "Error"
