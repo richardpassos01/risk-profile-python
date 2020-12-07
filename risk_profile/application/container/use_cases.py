5bfbf65c6ce14e8dabfb9a5ea8512a13b405029d
richardpassos01 <richard.passos@hotmail.com>
2020-12-06 23:50:25 -0300

    feat: create use cases factory inside application

---
+++ risk_profile/application/container/use_cases.py
@@ -0,0 +1,4 @@
+from risk_profile.domain.suitability.use_cases import provide_risk_profile
+
+def create_provide_risk_profile():
+    return provide_risk_profile.ProvideRiskProfile()
--- risk_profile/application/controllers/RiskProfileViewSet.py
+++ risk_profile/application/controllers/RiskProfileViewSet.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from risk_profile.domain.suitability.use_cases import provide_risk_profile
+from risk_profile.application.container import use_cases
 from risk_profile.domain.suitability.serializer import SuitabilitySerializer
 from rest_framework.response import Response
 from risk_profile.domain.suitability import Suitability as Model
@@ -9,8 +9,8 @@ class RiskProfileViewSet(viewsets.ModelViewSet):
     serializer_class = SuitabilitySerializer
 
     def create(self, request):
-        use_case = provide_risk_profile.ProvideRiskProfile()
-        risk_profile = use_case.execute(request.data)
+        provide_risk_profile = use_cases.create_provide_risk_profile()
+        risk_profile = provide_risk_profile.execute(request.data)
        
-        serializer = SuitabilitySerializer(queryset, many=True)
+        serializer = SuitabilitySerializer(self.queryset, many=True)
         return Response(serializer.data)
